4c646478dc6f9c638a2eef1f0d076fe1e8678135
khayalgeek <xeyalbinnetov02@gmail.com>
2024-12-14 23:41:08 +0400

    created cors policy extension method and added program.cs

--- PMS/Extensions/ServiceExtensions.cs
+++ PMS/Extensions/ServiceExtensions.cs
@@ -1,17 +1,6 @@
 namespace PMS.Extensions;
 
-public static class ServiceExtensions
+public class ServiceExtensions
 {
-    public static void ConfigureCors(this IServiceCollection services)
-    {
-        services.AddCors(options =>
-        {
-            options.AddPolicy("CorsPolicy", builder =>
-            {
-                builder.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            });
-        });
-    }
+    
 }
