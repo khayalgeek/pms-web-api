d07c7703c113599cd45d42882b7a1d66147c9bf7
khayalgeek <xeyalbinnetov02@gmail.com>
2024-12-17 21:52:11 +0400

    created project repository

--- Contracts/IProjectRepository.cs
+++ Contracts/IProjectRepository.cs
@@ -1,6 +1,12 @@
+using Entities.Models;
+
 namespace Contracts;
 
 public interface IProjectRepository
 {
-    
+    IEnumerable<Project> GetAllProjects(bool trackChanges);
+    Project? GetProjectById(Guid projectId, bool trackChanges);
+    void CreateProject(Project project);
+    void UpdateProject(Project project);
+    void DeleteProject(Project project);
 }
--- Repository/ProjectRepository.cs
+++ Repository/ProjectRepository.cs
@@ -1,6 +1,29 @@
+using Contracts;
+using Entities.Models;
+
 namespace Repository;
 
-public class ProjectRepository
+public class ProjectRepository: RepositoryBase<Project>, IProjectRepository
 {
-    
+    public ProjectRepository(RepositoryContext dbContext) : base(dbContext)
+    {
+    }
+
+    public IEnumerable<Project> GetAllProjects(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(p=>p.Name)
+            .ToList();
+
+    public Project? GetProjectById(Guid projectId, bool trackChanges) =>
+        FindByCondition(p=>p.Id.Equals(projectId), trackChanges)
+            .SingleOrDefault();
+
+    public void CreateProject(Project project) => 
+        Create(project);
+
+    public void UpdateProject(Project project) =>
+        Update(project);
+   
+    public void DeleteProject(Project project) => 
+        Delete(project);
 }
